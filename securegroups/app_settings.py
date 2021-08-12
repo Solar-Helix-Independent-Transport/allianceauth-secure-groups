77124435861c0c66a5ead9838fdea68810b54408
AaronKable <aaronkable@gmail.com>
2021-08-12 13:23:11 +0800

    dupe setting

--- securegroups/app_settings.py
+++ securegroups/app_settings.py
@@ -5,5 +5,5 @@ def discord_bot_active():
     return "aadiscordbot" in settings.INSTALLED_APPS
 
 
-DISCORD_BOT_COGS = getattr(settings, 'DISCORD_BOT_COGS', ["securegroups.cogs.groupcheck",
-                                                          ])
+DISCORD_BOT_COGS = getattr(settings, 'SG_DISCORD_BOT_COGS', ["securegroups.cogs.groupcheck",
+                                                             ])
