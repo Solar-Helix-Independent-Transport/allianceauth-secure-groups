331addb94147769e9620d291e5a7e12f661423e2
Aaron Kable <aaronkable@gmail.com>
2025-04-11 20:01:24 +0800

    vbump

--- securegroups/__init__.py
+++ securegroups/__init__.py
@@ -1,5 +1,5 @@
 """
 On its own this app does very little! However it leverages any module that is capable of providing a filter. Giving you the ability to add a very wide range of automatic filtration options your groups.
 """
-__version__ = "0.7.1"
+__version__ = "0.8.0"
 __title__ = "SecureGroups"
