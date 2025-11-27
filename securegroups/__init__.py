46be65976a1373d8251df00f65aad0db10ad1355
Bates Larsson <bateslarsson@gmail.com>
2025-11-27 15:48:06 +0000

    Updated version to 0.8.2

--- securegroups/__init__.py
+++ securegroups/__init__.py
@@ -1,5 +1,5 @@
 """
 On its own this app does very little! However it leverages any module that is capable of providing a filter. Giving you the ability to add a very wide range of automatic filtration options your groups.
 """
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 __title__ = "SecureGroups"
